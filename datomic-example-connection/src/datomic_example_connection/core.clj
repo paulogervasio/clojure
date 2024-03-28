c34d5acac51d631528fa8d2f7d835515aa38b2cf
Paulo Gervasio <pgervasiosousa@gmail.com>
2024-03-28 12:55:33 -0300

    Persist a new entity using a function

--- datomic-example-connection/src/datomic_example_connection/core.clj
+++ datomic-example-connection/src/datomic_example_connection/core.clj
@@ -1,15 +1,20 @@
-(ns datomic-example-connection.core)
+(ns datomic-example-connection.core
+  (:require [datomic.api :as d]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
 
-
-(require '[datomic.api :as d])
 (println "connect to database...")
 (def db-uri "datomic:dev://172.17.0.2:4334/hello?password=unsafe")
 (def conn (d/connect db-uri))
-(println "trx")
-@(d/transact conn [{:db/doc "Hello world999"}])
-(println "done")
+
+(defn insert-new-entity
+  "Insert a new entity into Datomic"
+  [x]
+  (println "insert a new trx")
+  @(d/transact conn [{:db/doc "Hello world10000000000000000123"}])
+)
+
+(insert-new-entity "xxxx")
